9fb9657bce8e2e2c85c753744a6a7b0667cb4773
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2025-01-31 08:52:55 +0530

    finished setting up mailtrap

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+import { sendVerificationEmail } from '../mailtrap/emails.js';
 
 export const signup = async (req, res) => {
     const {email, password, name, username} = req.body;
@@ -32,6 +33,7 @@ export const signup = async (req, res) => {
 
         await user.save();
         generateTokenAndSetCookie(res, user._id);
+        await sendVerificationEmail(user.email, verificationToken);
 
         res.status(201).json({success: true, message: 'User created successfully', user: {
             ...user._doc, password: undefined
--- server/src/mailtrap/mailtrap.config.js
+++ server/src/mailtrap/mailtrap.config.js
@@ -1,32 +1,11 @@
-import { MailtrapClient } from "mailtrap-client";
+import { MailtrapClient } from "mailtrap";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const TOKEN = process.env.MAILTRAP_TOKEN;
-const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+export const mailtrapClient = new MailtrapClient({token: process.env.MAILTRAP_TOKEN});
 
-const client = new MailtrapClient({
-  token: TOKEN,
-  endpoint: ENDPOINT
-});
-
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.com",
-  name: "MailTrap",
+  name: "MailTrap Test",
 };
-const recipients = [
-  {
-    email: "vaibhavchitranshbdn@gmail.com",
-  }
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
